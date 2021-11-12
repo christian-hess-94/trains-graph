ced12df3be11bc6e3d45942c5456bb4ae1e08c89
Christian Hess <christian.hess.invillia@tembici-consult.com>
2021-11-12 13:25:26 -0300

    Updates

--- src/providers.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TrainsProvider } from "./context/trains.context";
-
-const Providers: React.FC = ({ children }) => {
-  return <TrainsProvider>{children}</TrainsProvider>;
-};
-
-export default Providers;
